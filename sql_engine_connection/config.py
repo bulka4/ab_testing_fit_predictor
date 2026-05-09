d115f5fbf62eca08ae19ed67b73a397d1a1a8208
marcin bułka <mbulka44@gmail.com>
2026-05-09 18:02:58 +0200

    first commit

---
+++ sql_engine_connection/config.py
@@ -0,0 +1,8 @@
+# example configuration
+cfg = {
+  'host': 'analytics-testdb.ctmikfo5p3gj.us-east-1.rds.amazonaws.com',
+  'user': 'ssp_test',
+  'password': 'Iev4aiDu',
+  'port': '5432',
+  'db': 'ssp'
+}
